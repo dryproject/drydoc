6e40a3182180684058d2a48353a529a5e20b39cf
Arto Bendiken <arto@dryproject.org>
2019-03-11 18:51:47 +0200

    Created a skeleton for the Java plugin.

---
+++ java/src/ParseJava.java
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package org.drydoc;
+
+public class ParseJava {
+  public static void main(final String[] args) {
+    System.out.println("OK"); // TODO
+  }
+}
