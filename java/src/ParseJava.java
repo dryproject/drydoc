a32631f240f3e1caacbb3a19956ca23ca3c4ce85
Arto Bendiken <arto@dryproject.org>
2019-03-11 19:51:44 +0200

    Began work on parsing Java files.

--- java/src/ParseJava.java
+++ java/src/ParseJava.java
@@ -2,8 +2,57 @@
 
 package org.drydoc;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/** ParseJava */
 public class ParseJava {
-  public static void main(final String[] args) {
-    System.out.println("OK"); // TODO
+
+  /** ParseJava.main(String[]) */
+  public static void main(final String[] args) throws IOException {
+    final JavaParser parser = new JavaParser();
+
+    for (final String arg : args) {
+      System.err.printf(">>> Parsing: %s\n", arg);
+      try {
+        final FileInputStream input = new FileInputStream(arg);
+        final ParseResult<CompilationUnit> result = parser.parse(input);
+        result.ifSuccessful(unit -> unit.accept(new Visitor(), null));
+      }
+      catch (final FileNotFoundException error) {
+        System.err.printf(">>> ERROR: %s\n", error.getMessage());
+        continue; // skip this file
+      }
+    }
+  }
+
+  private static class Visitor extends VoidVisitorAdapter<Void> {
+
+    @Override
+    public void visit(final PackageDeclaration node, final Void arg) {
+      System.out.printf("%s\n", node.getName()); // TODO
+      super.visit(node, arg);
+    }
+
+    @Override
+    public void visit(final ClassOrInterfaceDeclaration node, final Void arg) {
+      System.out.printf("\t%s\n", node.getName()); // TODO
+      super.visit(node, arg);
+    }
+
+    @Override
+    public void visit(final MethodDeclaration node, final Void arg) {
+      System.out.printf("\t\t%s\n", node.getName()); // TODO
+      super.visit(node, arg);
+    }
   }
 }
